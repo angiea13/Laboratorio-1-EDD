08190b0454f4303b494985d14421ab0fafe469c3
Angie <118225016+angiea13@users.noreply.github.com>
2026-09-08 22:16:28 +0000

    Base de interfaz implementadagit add .

---
+++ src/main/java/core/test/HelloController.java
@@ -0,0 +1,14 @@
+package core.test;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label label;
+
+    @FXML
+    public void initialize() {
+        label.setText("Funciona");
+    }
+}
---
+++ src/main/java/core/test/TestUI.java
@@ -0,0 +1,19 @@
+package core.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("hello-view.fxml")));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
