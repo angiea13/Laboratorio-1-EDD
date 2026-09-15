81da3a5f9c8bddd47c179bc2754301ba9186b413
Angie <angiearteta95@gmail.com>
2026-09-14 19:32:08 -0500

    Cambiada disposición de JavaFX y comenzada implementación real real real del Canva: https://canva.link/if0u36qxo7qvnfd

--- src/main/java/core/test/TestUI.java
+++ src/main/java/core/test/TestUI.java
@@ -1,15 +1,62 @@
 package core.test;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class TestUI extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("hello-view.fxml")));
+    public void start(Stage stage) {
+        
+        Pane pantallaInicio = new Pane();
+        Label horaFecha = new Label();
+
+        LocalDateTime fechaHora = LocalDateTime.now();
+        DateTimeFormatter formatoFechaHora = DateTimeFormatter.ofPattern("dd MMM de yyyy, HH:mm:ss");
+        String textoFecha = fechaHora.format(formatoFechaHora);
+        
+        horaFecha.setText(textoFecha);
+
+        horaFecha.setLayoutX(967);
+        horaFecha.setLayoutY(17);
+
+        pantallaInicio.getChildren().add(horaFecha);
+
+
+        Color bgColor = Color.web("#E7E7E7");
+
+        Label label = new Label("¡Bienvenido al Sistema de Información STRUCTART!");
+
+        VBox root = new VBox(10);
+        root.setAlignment(javafx.geometry.Pos.CENTER);
+
+        root.setBackground(
+            new Background(
+            new BackgroundFill(bgColor, null, null)
+            )
+        );
+
+        root.getChildren().addAll(label, pantallaInicio);
+
+        Scene scene = new Scene(root);
+
         stage.setTitle("STRUCTART Integrated Systems (SARIS)");
+        stage.setResizable(false);
+
+        stage.setWidth(1440);
+        stage.setHeight(810);
+
         stage.setScene(scene);
         stage.show();
     }
